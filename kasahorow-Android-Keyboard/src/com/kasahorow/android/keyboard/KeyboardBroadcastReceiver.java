f2cb5ed9b551627b9cc534730a917ffd87b68b70
Henry Addo <addhen@linuxmail.org>
2013-06-11 21:59:31 +0900

    Fix aapt bug.
    See this post for details
    http://devmaze.wordpress.com/2011/05/22/the-case-of-android-libraries-and-custom-xml-attributes-part-2/

---
+++ kasahorow-Android-Keyboard/src/com/kasahorow/android/keyboard/KeyboardBroadcastReceiver.java
@@ -0,0 +1,14 @@
+package com.kasahorow.android.keyboard;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class KeyboardBroadcastReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context arg0, Intent arg1) {
+
+    }
+
+}
